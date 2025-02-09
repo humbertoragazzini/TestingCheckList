3f8224613edb315e55223035ac4c1902ac35868c
humbertoragazzini <beto_vicio@hotmail.com>
2025-02-09 21:48:55 +0000

    added small function to add check or uncheck

--- src/components/ItemCheck.tsx
+++ src/components/ItemCheck.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 export default function ItemChack() {
+	const [isChecked, setIsChecked] = useState(false);
 	return (
 		<div className="grid grid-cols-12 text-black font-bold">
 			<div className="bg-red-200 border-1 border-red-300 p-3 col-span-1">1</div>
@@ -8,9 +11,17 @@ export default function ItemChack() {
 			<div className="bg-red-200 border-1 border-red-300 p-3 col-span-5">
 				Comment
 			</div>
-			<div className="bg-red-200 border-1 border-red-300 p-3 col-span-2">
-				<button className="flex justify-center items-center">
-					Checked? <span className="block ml-4 w-4 h-4 bg-white"></span>
+			<div
+				onClick={() => {
+					setIsChecked(!isChecked);
+				}}
+				className="bg-red-200 border-1 border-red-300 p-3 col-span-2 cursor-pointer"
+			>
+				<button className="flex justify-center items-center cursor-pointer">
+					Checked?{" "}
+					<span className="flex justify-center items-center ml-4 w-5 h-5 bg-white">
+						{isChecked && <>X</>}
+					</span>
 				</button>
 			</div>
 		</div>
